efe689afcfe2bd8edaebacad5e613603ff2e5b02
Revath S Kumar <rsk@revathskumar.com>
2013-08-30 02:20:10 +0530

    boilerplate

---
+++ scripts/ca2c3692.main.js
@@ -0,0 +1,1 @@
+window.yobackbone={Models:{},Collections:{},Views:{},Routers:{},init:function(){"use strict";console.log("Hello from Backbone!")}},$(document).ready(function(){"use strict";yobackbone.init()}),this.JST=this.JST||{};
